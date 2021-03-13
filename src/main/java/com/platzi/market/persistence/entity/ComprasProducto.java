0ee31cb2aae0ac0a1f9649b70b75e9806862bc14
Luis Angel <santosangel748@gmail.com>
2021-03-13 13:00:44 -0600

    Mapeando las tablas como clases y creando entitys con clave primaria compuesta

---
+++ src/main/java/com/platzi/market/persistence/entity/ComprasProducto.java
@@ -0,0 +1,51 @@
+package com.platzi.market.persistence.entity;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "compras_productos")
+public class ComprasProducto {
+
+    @EmbeddedId
+    private ComprasProductosPK id;
+
+    private Integer cantidad;
+
+    private Double total;
+
+    private Boolean estado;
+
+    public ComprasProductosPK getId() {
+        return id;
+    }
+
+    public void setId(ComprasProductosPK id) {
+        this.id = id;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Double getTotal() {
+        return total;
+    }
+
+    public void setTotal(Double total) {
+        this.total = total;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+}
---
+++ src/main/java/com/platzi/market/persistence/entity/ComprasProductosPK.java
@@ -0,0 +1,31 @@
+package com.platzi.market.persistence.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.io.Serializable;
+
+@Embeddable
+public class ComprasProductosPK implements Serializable {
+
+    @Column(name = "id_compra")
+    private Integer idCompra;
+
+    @Column(name = "id_producto")
+    private String idProducto;
+
+    public Integer getIdCompra() {
+        return idCompra;
+    }
+
+    public void setIdCompra(Integer idCompra) {
+        this.idCompra = idCompra;
+    }
+
+    public String getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(String idProducto) {
+        this.idProducto = idProducto;
+    }
+}
